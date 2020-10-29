38117717c8db8760c578cae4886d5a70d24f1ec2
Teker78 <miguel00rg@hotmail.com>
2020-10-29 20:46:58 +0100

    Añado practica 9 y 10 terminadas

---
+++ Desarrollo Web Cliente/Tema 3/Practica 10/js/js.js
@@ -0,0 +1,63 @@
+// MODELO
+var peliculas = [
+    {titulo: "Superlópez",   director: "Javier Ruiz Caldera", estreno: "2018"},
+    {titulo: "E.T.",         director: "Steven Spielberg",    estreno: "1982"},
+    {titulo: "Interstellar", director: "Christopher Nolan",   estreno: "2014"}
+];
+
+// VISTAS
+function indexView (pelis) {
+    var    i=0,   html = "<ul>";
+    while(i < pelis.length) {
+      html = html + `<li id="show" data-my-id="${i}"> ${pelis[i].titulo}<button id="delete" data-my-id="${i}">Borrar</button></li>`;
+      i = i + 1;
+    };
+    return html + `</ul> <button id="new">Crear</button>`;
+};
+
+function showView (peli) {
+    return `
+    La película <b> ${peli.titulo}</b>, estrenada
+    en el año <b> ${peli.estreno}</b>, fue
+    dirigida por <b> ${peli.director}</b>.
+
+    <p><button id="index"> Volver </button></p>`
+}
+
+function newView() {
+    return `<p>Introducir nueva pelicula:</p>
+    <p>Título: <input type="text" id="tituloPeli"></p>
+    <p>Director: <input type="text" id="directorPeli"></p>
+    <p>Año Estreno: <input type="text" id="estrenoPeli"></p>
+    <button id="create">Añadir</button>`
+};
+
+// CONTROLADORES
+function indexContr() { document.getElementById("main").innerHTML = indexView(peliculas);};
+function showContr(i) { document.getElementById("main").innerHTML = showView(peliculas[i]);};
+function deleteContr(i) {
+    peliculas.splice(i, 1);
+    indexContr();
+};
+function newContr() { document.getElementById("main").innerHTML = newView(); };
+
+function createContr() {
+
+    let titulo = document.getElementById("tituloPeli").value;
+    let director = document.getElementById("directorPeli").value;
+    let estreno = document.getElementById("estrenoPeli").value;
+
+    peliculas.push({titulo: titulo, director: director,   estreno: estreno});
+    
+    indexContr();
+};
+
+// EVENTOS
+document.addEventListener('DOMContentLoaded', ev => indexContr());
+document.addEventListener('click', ev => {
+    if      (ev.target.matches('#index')) indexContr();
+    else if (ev.target.matches('#show'))  showContr(ev.target.dataset.myId);
+    else if (ev.target.matches('#delete')) deleteContr(ev.target.dataset.myId);
+    else if (ev.target.matches('#new')) newContr();
+    else if (ev.target.matches('#create')) createContr();
+})
--- Desarrollo Web Cliente/Tema 3/Practica 9/js/js.js
+++ Desarrollo Web Cliente/Tema 3/Practica 9/js/js.js
@@ -5,7 +5,7 @@ var peliculas = ["Superlópez", "E.T.", "Interstellar"];
 function indexView(pelis) {
     var i = 0, html = "<ul>";
     while (i < pelis.length) {
-        html = html + `<li> ${pelis[i]}<button id="delete" data-my-id="${i}">Borrar</button></li>`;
+        html = html + `<li> ${pelis[i]}<button id="delete" data-my-id="${i}">Borrar</button><button id="edit" data-my-id="${i}">Editar</button></li>`;
         i = i + 1;
     };
     return html + `</ul> <button id="new">Crear</button>`;
@@ -15,9 +15,15 @@ function newView() {
     return `Introducir nueva pelicula: <input type="text" id="peli"> <button id="create">Añadir</button>`
 };
 
+function editView(index) {
+  return `<p>Pelicula número: <strong id="indexPeli">${index}</strong> </p>Escriba el nuevo nombre para la pelicula: ${peliculas[index]} <input type="text" id="nuevoTitulo"><button id="editTitle">Editar</button>`;
+}
+
 // CONTROLADORES
 function indexContr() { document.getElementById("main").innerHTML = indexView(peliculas); };
 function newContr() { document.getElementById("main").innerHTML = newView(); };
+function editContr(index) { document.getElementById("main").innerHTML = editView(index); };
+
 function createContr() {
     peliculas.push(document.getElementById("peli").value);
     indexContr();
@@ -26,6 +32,11 @@ function deleteContr(i) {
     peliculas.splice(i, 1);
     indexContr();
 };
+function editTitleContr() {
+    let indexPeliculas = document.getElementById("indexPeli").innerHTML;
+    peliculas[indexPeliculas] = document.getElementById("nuevoTitulo").value;
+    indexContr();
+}
 
 // EVENTOS
 document.addEventListener('DOMContentLoaded', ev => indexContr());
@@ -33,4 +44,6 @@ document.addEventListener('click', ev => {
     if (ev.target.matches('#new')) newContr();
     else if (ev.target.matches('#create')) createContr();
     else if (ev.target.matches('#delete')) deleteContr(ev.target.dataset.myId);
+    else if (ev.target.matches('#edit')) editContr(ev.target.dataset.myId);
+    else if (ev.target.matches('#editTitle')) editTitleContr(ev.target.dataset.myId);
 })
