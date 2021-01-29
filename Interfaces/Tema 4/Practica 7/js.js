524a5ecc84291e29869220a00c8626cea72b2f0a
Teker78 <miguel00rg@hotmail.com>
2021-01-29 13:10:47 +0100

    Añado practica 7 completada

--- Interfaces/Tema 4/Practica 7/js.js
+++ Interfaces/Tema 4/Practica 7/js.js
@@ -1,5 +1,19 @@
 let blinkNum = 0;
 
+
+function blink(element) {
+    if (blinkNum == 0) {
+        blinkNum = 1;
+        $("#jQueryText").css("color", "yellow");
+    } else {
+        blinkNum = 0;
+        $("#jQueryText").css("color", "red");
+    }
+}
+
+
+
+
 $(document).ready(function () {
     //1
     $("#check").change(function () {
@@ -17,19 +31,35 @@ $(document).ready(function () {
     $("#añadir").click(function (e) { 
         e.preventDefault();
         let val = $("#newLi").val();
-        console.log(val);
+        $("#list").append($("<li></li>").text(val));
+    });
+
+    //4
+
+    $(".imgTable").mouseenter(function () { 
+        $(".imgTable").css("opacity", "0.4");
+        $(this).css("opacity", "1");
+
+        $(this).css("transform", "scale(1.1)");
+    });
+    $(".imgTable").mouseleave(function () {
+        $(".imgTable").css("opacity", "1");
+        $(this).css("transform", "scale(1)");
+    });
+
+    //5
+    $(".pregunta").click(function (e) { 
+        $(".respuestaPreguntas").css("display", "none");
+        $(this).next().css("display", "block");
+    });
+
+    //6
+    $("#block").mouseenter(function () {
+        if ($("#block").css("float") == "left") {
+            $("#block").css("float", "right");
+        } else {
+            $("#block").css("float", "left");
+        }
     });
 
 });
-
-
-
-function blink(element) {
-    if (blinkNum == 0) {
-        blinkNum = 1;
-        $("#jQueryText").css("color", "yellow");
-    } else {
-        blinkNum = 0;
-        $("#jQueryText").css("color", "red");
-    }
-}
