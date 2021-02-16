e8c0750bcf6fff29bea501dd2572e3f886b95aaa
Miguel <miguel00rg@hotmail.com>
2021-02-16 19:48:41 +0100

    Añado prograso en practica 1 tema 5, sin completar

--- Interfaces/Tema 5/Practica 1/js.js
+++ Interfaces/Tema 5/Practica 1/js.js
@@ -1,14 +1,49 @@
+//Añade  el metodo de validación para meter regex en el form.validate()
+$.validator.addMethod(
+  "regex",
+  function(value, element, regexp) {
+    var re = new RegExp(regexp);
+    return this.optional(element) || re.test(value);
+  },
+  "Please check your input."
+);
+
+
+//Añadir el datepicker al input de date
+
+
 $(document).ready(function() {
     $("#validate").validate({
       rules: {
         name : {
           required: true,
-          minlength: 3
+          regex: "^[a-zA-Z]+$"
+        },
+        lastName : {
+          required: true,
+          regex: "^[a-zA-Z]+$"
+        },
+        dni : {
+          required: true,
+          regex : "^\d{8}[a-zA-Z]{1}$"
+        }
+      },
+      messages : {
+        name : {
+          required : "Este campo es necesario",
+          regex : "Solo se admiten letras en este campo"
+        },
+        lastName : {
+          required : "Este campo es necesario",
+          regex : "Solo se admiten letras en este campo"
+        },
+        dni : {
+          required : "Este campo es necesario",
+          regex : "Escriba un DNI válido ej: '12345678Z'"
         }
       }
     });
   });
-
 /*
 $(document).ready(function() {
   $("#basic-form").validate({
